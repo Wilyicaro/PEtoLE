5f46dff36180a6abb245da5e57c9f07978d1dab2
Wilyicaro <icarokauanbonfim@hotmail.com>
2025-06-21 12:19:06 -0300

    -Fixed touchscreen.png being in the repo assets -Fixed SoundData.cpp still looking for the old sounds

--- source/client/sound/SoundData.cpp
+++ source/client/sound/SoundData.cpp
@@ -7,14 +7,3 @@
  ********************************************************************/
 
 #include "SoundData.hpp"
-
-// --------------------------------------------------------------------
-// WARNING! If you have an error here it is most likely because you did
-// not grab the sound data from a working Minecraft PE 0.1.3 .apk.
-// 
-// Check the readme for a guide on how to extract game sounds from the
-// Minecraft PE 0.1.3 .apk file.
-// --------------------------------------------------------------------
-#ifndef MISSING_SOUND_DATA
-#include "../../sound_data/sounds.h"
-#endif
